3d76b8d4db3a1cb0cef1b872a711d87a449bea86
KaalaAadmi <arnav264@gmail.com>
2023-08-25 22:39:17 +0100

    functions

--- functions/myfunc.js
+++ functions/myfunc.js
@@ -1,5 +1,6 @@
 function addTwo(num) {
     return num + 2;
+    // return "hello"
 }
 addTwo(5);
 function getUpper(val) {
@@ -11,3 +12,23 @@ var loginUser = function (name, email, password, isPaid) {
 };
 loginUser("john", "j@j.com", "1234");
 getUpper("hello");
+function getValues(myVal) {
+    if (myVal > 5) {
+        return true;
+    }
+    return "200 OK";
+}
+var getHello = function (s) {
+    return "";
+};
+// const heros=["thor",'batman','superman']
+var heros = [1, 2, 3];
+heros.map(function (hero) {
+    return "".concat(hero, " is a hero");
+});
+function consoleError(errmsg) {
+    console.error(errmsg);
+}
+function handleError(errmsg) {
+    console.error(errmsg);
+}
--- functions/myfunc.ts
+++ functions/myfunc.ts
@@ -1,5 +1,6 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
 	return num + 2;
+	// return "hello"
 }
 addTwo(5);
 
@@ -23,3 +24,29 @@ let loginUser = (
 
 loginUser("john", "j@j.com", "1234");
 getUpper("hello");
+
+function getValues(myVal: number) {
+	if (myVal > 5) {
+		return true;
+	}
+	return "200 OK";
+}
+
+const getHello = (s: string): string => {
+	return "";
+};
+
+// const heros=["thor",'batman','superman']
+const heros = [1, 2, 3];
+
+heros.map((hero): string => {
+	return `${hero} is a hero`;
+});
+
+function consoleError(errmsg: string):void {
+	console.error(errmsg);
+}
+
+function handleError(errmsg: string):void {
+	console.error(errmsg);
+}
