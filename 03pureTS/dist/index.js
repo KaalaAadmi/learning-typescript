2bb9752f9f152df74d4bba8beadc0476e06125b2
KaalaAadmi <arnav264@gmail.com>
2023-08-26 23:28:04 +0100

    private and public

--- 03pureTS/dist/index.js
+++ 03pureTS/dist/index.js
@@ -2,7 +2,7 @@
 // pure ts start
 // console.log("typescript is here!")
 // console.log("typescript is amazing!")
-// next video
+// classes
 class User {
     constructor(email, name) {
         this.city = "mumbai";
@@ -13,3 +13,23 @@ class User {
 const arnav = new User("a@a.com", "arnav");
 // arnav.city=2 // error
 // arnav.city="dublin" // ok // error when city is declared as readonly.
+// private and public - default is public. to declare private in js, use # before the variable name
+// class User2 {
+// 	public email: string;
+// 	name: string;
+// 	private readonly city: string = "mumbai";
+// 	constructor(email: string, name: string) {
+// 		this.email = email;
+// 		this.name = name;
+// 	}
+// }
+// const arnav1 = new User2("a@a.com", "arnav");
+// arnav1.city // error
+class User2 {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "mumbai";
+    }
+}
+const arnav1 = new User2("a@a.com", "arnav");
--- 03pureTS/src/index.ts
+++ 03pureTS/src/index.ts
@@ -2,7 +2,7 @@
 // console.log("typescript is here!")
 // console.log("typescript is amazing!")
 
-// next video
+// classes
 class User {
 	email: string;
 	name: string;
@@ -16,3 +16,28 @@ class User {
 const arnav = new User("a@a.com", "arnav");
 // arnav.city=2 // error
 // arnav.city="dublin" // ok // error when city is declared as readonly.
+
+// private and public - default is public. to declare private in js, use # before the variable name
+// class User2 {
+// 	public email: string;
+// 	name: string;
+// 	private readonly city: string = "mumbai";
+// 	constructor(email: string, name: string) {
+// 		this.email = email;
+// 		this.name = name;
+// 	}
+// }
+
+// const arnav1 = new User2("a@a.com", "arnav");
+// arnav1.city // error
+
+class User2 {
+	readonly city: string = "mumbai";
+	constructor(
+		public email: string,
+		public name: string
+	) // private userId:string,
+	{}
+}
+
+const arnav1 = new User2("a@a.com", "arnav");
