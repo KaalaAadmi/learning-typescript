d2593b2e0023bcf53ffe789b30e2484f8ada3dc1
KaalaAadmi <arnav264@gmail.com>
2023-08-26 19:28:18 +0100

    pure ts classes

--- 03pureTS/dist/index.js
+++ 03pureTS/dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log("typescript is here!");
-console.log("typescript is amazing!");
+// pure ts start
+// console.log("typescript is here!")
+// console.log("typescript is amazing!")
+// next video
+class User {
+    constructor(email, name) {
+        this.city = "mumbai";
+        this.email = email;
+        this.name = name;
+    }
+}
+const arnav = new User("a@a.com", "arnav");
+// arnav.city=2 // error
+// arnav.city="dublin" // ok // error when city is declared as readonly.
--- 03pureTS/src/index.ts
+++ 03pureTS/src/index.ts
@@ -1,2 +1,18 @@
-console.log("typescript is here!")
-console.log("typescript is amazing!")
+// pure ts start
+// console.log("typescript is here!")
+// console.log("typescript is amazing!")
+
+// next video
+class User {
+	email: string;
+	name: string;
+	readonly city: string = "mumbai";
+	constructor(email: string, name: string) {
+		this.email = email;
+		this.name = name;
+	}
+}
+
+const arnav = new User("a@a.com", "arnav");
+// arnav.city=2 // error
+// arnav.city="dublin" // ok // error when city is declared as readonly.
