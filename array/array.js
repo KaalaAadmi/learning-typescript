9b73b313962ae9e1a9d2841eadf0dd9c6533d45b
KaalaAadmi <arnav264@gmail.com>
2023-08-26 14:35:36 +0100

    array

---
+++ array/array.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeroes = [];
+// const heroPower:number[]=[]
+var heroPower = [];
+superHeroes.push("spiderman");
+heroPower.push(100);
+// const allUsers:User[]=[];
+var allUsers = [];
+allUsers.push({ name: "hello", isActive: true });
+var MLModels = [[255, 255, 255]];
---
+++ array/array.ts
@@ -0,0 +1,19 @@
+const superHeroes: string[] = [];
+// const heroPower:number[]=[]
+const heroPower: Array<number> = [];
+
+superHeroes.push("spiderman");
+heroPower.push(100);
+
+type User = {
+	name: string;
+	isActive: boolean;
+};
+
+// const allUsers:User[]=[];
+const allUsers: Array<User> = [];
+allUsers.push({ name: "hello", isActive: true });
+
+const MLModels: number[][] = [[255, 255, 255]];
+
+export {};
