12770d35a09e4c8b0f44db4acf58372f67dea234
KaalaAadmi <arnav264@gmail.com>
2023-08-27 03:23:33 +0100

    instanceof and type predicates

--- 03pureTS/dist/detection.js
+++ 03pureTS/dist/detection.js
@@ -32,3 +32,28 @@ function isAdminAccount(account) {
         return account.isAdmin;
     }
 }
+// instanceof and type predicates
+// instanceof can only be used when the new keyword can be used to create an object. e.g.: Date.
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+        return;
+    }
+    else {
+        console.log(x.toUpperCase());
+        return;
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
--- 03pureTS/src/detection.ts
+++ 03pureTS/src/detection.ts
@@ -45,3 +45,33 @@ function isAdminAccount(account: User | Admin) {
 		return account.isAdmin;
 	}
 }
+
+// instanceof and type predicates
+// instanceof can only be used when the new keyword can be used to create an object. e.g.: Date.
+function logValue(x: Date | string) {
+	if (x instanceof Date) {
+		console.log(x.toUTCString());
+		return;
+	} else {
+		console.log(x.toUpperCase());
+		return;
+	}
+}
+
+// type predicates
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+	return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+	if (isFish(pet)) {
+		pet;
+		return "fish food";
+	} else {
+		pet;
+		return "bird food";
+	}
+}
